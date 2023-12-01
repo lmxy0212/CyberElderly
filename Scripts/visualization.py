e4aba1d394fb97bbeba50ba262851821f73f5c54
Manxueying Li <manxueying.li@columbia.edu>
2023-12-01 15:01:32 -0500

    calculation for ICE

--- Scripts/visualization.py
+++ Scripts/visualization.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.signal import welch
 from collections import deque
 import threading
+import time
 
 # Configuration
 ip = "0.0.0.0"
@@ -22,16 +23,25 @@ eeg_bands = {'Delta': (0.5, 4),
 # Circular buffer to store EEG data
 eeg_buffer = deque(maxlen=n_samples)
 
-# Buffer for storing last 10 seconds of alpha averages
+# Buffers for storing last 10 seconds of band averages
 alpha_buffer = deque(maxlen=10)
-prev = 0
+beta_buffer = deque(maxlen=10)
+theta_buffer = deque(maxlen=10)
 
 def eeg_handler(address: str, *args):
-    eeg_buffer.extend(args)
+    # Assuming TP9 data is the first value in args
+    tp9_data = args[0]
+    eeg_buffer.append(tp9_data)
 
+def normalize(value, min_value, max_value):
+    # Scale ICE to 0-100 range
+    return 100 * (value - min_value) / (max_value - min_value)
 
 def process_eeg_data():
-    last_alpha_value = None
+    last_print_time = time.time()
+    # min_ice = # Define the minimum expected ICE value
+    # max_ice = # Define the maximum expected ICE value
+
     while True:
         if len(eeg_buffer) == n_samples:
             segment = np.array(list(eeg_buffer))
@@ -46,21 +56,28 @@ def process_eeg_data():
                 band_power_dB = 10 * np.log10(band_power / 1)  # Reference power is 1 μV²/Hz
                 band_powers[band] = band_power_dB
 
-            # Update alpha buffer
+            # Update alpha, beta, and theta buffers
             alpha_buffer.append(band_powers['Alpha'])
+            beta_buffer.append(band_powers['Beta'])
+            theta_buffer.append(band_powers['Theta'])
 
-            # Print if there is a change
-            if last_alpha_value is not None and band_powers['Alpha'] != last_alpha_value:
-                if last_alpha_value is not None and abs(band_powers['Alpha'] - last_alpha_value) > 5:
-                    print(f"Change in Alpha band power: {band_powers['Alpha']} dB")
+            current_time = time.time()
+            if current_time - last_print_time >= 10:
+                if len(alpha_buffer) == alpha_buffer.maxlen:
+                    avg_alpha = np.mean(alpha_buffer)
+                    avg_beta = np.mean(beta_buffer)
+                    avg_theta = np.mean(theta_buffer)
 
-            last_alpha_value = band_powers['Alpha']
+                    # Calculate ICE
+                    ice = avg_beta / (avg_alpha + avg_theta)
 
-            # Calculate and print 10-second average
-            if len(alpha_buffer) == alpha_buffer.maxlen:
-                ten_second_avg = np.mean(alpha_buffer)
-                # print(f"10-second average of Alpha band: {ten_second_avg}")
+                    # Normalize ICE
+                    # normalized_ice = normalize(ice, min_ice, max_ice)
+                    # normalized_ice = max(0, min(100, normalized_ice))  # Clamping to 0-100 range
 
+                    # print(f"Normalized Index of Cognitive Engagement (ICE): {normalized_ice}")
+                    print(f"Index of Cognitive Engagement (ICE) over previous 10 seconds: {ice}")
+                    last_print_time = current_time
 
 if __name__ == "__main__":
     dispatcher = dispatcher.Dispatcher()
